8bed08b1707bf30a4565863a4a61552186e947ba
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-29 17:19:49 +0200

    [UPD] Lanciati comandi npm init, npm i express, npm i mysql2, inseriti alias start e watch + lanciati comandi, creato file app.js, creato cartella public, importato immagini.

---
+++ app.js
@@ -0,0 +1,24 @@
+// Importazione express
+const express = require(`express`);
+
+// Inizializzazione variabile app con metodo express()
+const app = express();
+
+// Definizione numero di porta su cui il server rimane in ascolto
+const port = 3000;
+
+// Utilizzo del middleware per asset statici
+app.use(express.static(`public`));
+
+// Utilizzo del middleware per il parsing del body delle richieste
+app.use(express.json());
+
+// Definizione Entry Point
+app.get("/", (req, res) => {
+    res.send(`Movies API server`)
+});
+
+// Server in ascolto
+app.listen(port, () => {
+    console.log(`Server in ascolto alla porta ${port}`)
+});
